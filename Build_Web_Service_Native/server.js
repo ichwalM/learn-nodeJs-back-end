37ee80b6e1506d6cbe98371a7582a09db7fe850a
ichwalM <13020230049@student.umi.ac.id>
2025-06-28 16:28:43 +0800

    Body Request

--- Build_Web_Service_Native/server.js
+++ Build_Web_Service_Native/server.js
@@ -2,11 +2,36 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
-};
 
+    const { method } = request;
+
+    if (method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+
+    if (method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        });
+    }
+
+    if (method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+
+    if (method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+};
 
 const server = http.createServer(requestListener);
 
